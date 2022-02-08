38c08a3f93634bbdb88658c3c07bfbb75217358a
alp61 <alpozturk61@hotmail.com>
2022-02-08 14:28:15 +0300

    update projects about us done

--- src/routes/hakkimizda.svelte
+++ src/routes/hakkimizda.svelte
@@ -5,7 +5,7 @@
   
   <div class="container mx-auto flex m-4 border-4">
    
-    <img src="/nedenbiz.png" class="w-2/4 m-4 " alt="">
+    <img src="/neden_biz_erturk_mobilya.png" class="w-2/4 m-4 " alt="">
     <p class="m text-left  m-2 mt-6 p-4 ml-4 italic">1970’den günümüze sürekli güncellenen altyapısı, 1000m2 üretim alanına sahip atölyemizde kaliteli mobilya ürünleri üretmekteyiz. 50 yıla yakın tecrübesiyle, banka, otel, zincir cafe ve restaurant dekorasyonlarıyla birlikte, mutfak, banyo, ev dekorasyonu ve tadilat işlerini de müşteri beklentilerine uygun olarak başarıyla gerçekleştirmiştir. İşlevsellik, kalite ve müşteri memnuniyetini ilke edinen firmamız yaşam alanlarına mutluluk getirmeye devam etmektedir.</p>
     <h2 class="text-3xl font-serif m-4 text-right mt-60 ml-111 absolute p-12  opacity-60 italic ">Yarım asırlık tecrübe</h2>
   </div>
@@ -22,7 +22,7 @@
 
 
 <div class="container mx-auto flex m-4 border-4">
- <img src="/erturk_mobilya_uretim_neden.jpeg" class="w-2/4 m-4 " alt="">
+ <img src="/nedenbiz.png" class="w-2/4 m-4 " alt="">
   <p class="m text-left  m-2 mt-6 p-4 ml-4">Birçok farklı müşteri grubundan edindiğimiz deneyimlerle sektörde uzun yıllardır hizmet vermekteyiz. Müşteri mağduriyeti oluşmaması için de planlamadan montaja kaliteyi düşürmeden belli bir düzeyde üretim yapmaktayız. Montaj aşamasında titizlikle uygulanan mobilyalar, uzun yıllar keyifle kullanabileceğiniz ürünlere dönüşmektedir.</p>
   <h2 class="text-3xl font-serif m-4 text-right mt-60 ml-111 absolute p-14  opacity-60 italic ">Neden Biz</h2>
 </div>
