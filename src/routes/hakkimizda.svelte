7316aa5bae74474c0a8cd0c81023079b9f95b41a
alp61 <alpozturk61@hotmail.com>
2022-02-08 14:07:40 +0300

    update projects about us done

--- src/routes/hakkimizda.svelte
+++ src/routes/hakkimizda.svelte
@@ -10,7 +10,7 @@
     <h2 class="text-3xl font-serif m-4 text-right mt-60 ml-111 absolute p-12  opacity-60 italic ">Yarım asırlık tecrübe</h2>
   </div>
 
-  <div class="h-40 w-full text-center flex-wrap items-center justify-center align-middle bg-white"></div>
+
 
   <div class="container mx-auto flex m-4 border-4">
   <p class="m text-left  m-2 mt-6 p-4 ml-4">İnovatif tasarım çizgilerini takip ederek yatak odaları, yemek odaları, mutfak, banyo, TV üniteleri ve mobilya aksesuarlarını kalite standartlarına uygun olarak üretmekteyiz. Mobilya üretiminde mdflam, ağaç kaplama, lake, ahşap ve aksesuar ürünlerini müşterinin ihtiyaçlarına göre özenle seçiyor ve yüksek kalitede uyguluyoruz.</p>
@@ -18,12 +18,11 @@
   <h2 class="text-3xl font-serif text-right mt-60 absolute p-12  opacity-60 italic ml-4 ">Ne yapıyoruz</h2>
 </div>
 
-<div class="h-40 w-full text-center flex-wrap items-center justify-center align-middle bg-white"></div>
+
 
 
 <div class="container mx-auto flex m-4 border-4">
-   
-  <img src="/nedenbiz.png" class="w-2/4 m-4 " alt="">
+ <img src="/erturk_mobilya_uretim_neden.jpeg" class="w-2/4 m-4 " alt="">
   <p class="m text-left  m-2 mt-6 p-4 ml-4">Birçok farklı müşteri grubundan edindiğimiz deneyimlerle sektörde uzun yıllardır hizmet vermekteyiz. Müşteri mağduriyeti oluşmaması için de planlamadan montaja kaliteyi düşürmeden belli bir düzeyde üretim yapmaktayız. Montaj aşamasında titizlikle uygulanan mobilyalar, uzun yıllar keyifle kullanabileceğiniz ürünlere dönüşmektedir.</p>
   <h2 class="text-3xl font-serif m-4 text-right mt-60 ml-111 absolute p-14  opacity-60 italic ">Neden Biz</h2>
 </div>
