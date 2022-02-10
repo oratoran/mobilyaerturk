26767c6d31bf0bfc752fe61709a9d61e80609ceb
Ozgur Dincer <ozgurdincer@kentajans.com>
2022-02-10 14:56:50 +0300

    add responsivity

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,6 +1,6 @@
-  <header class="flex w-full items-center justify-between px-6 py-3 pt-4 bg-gray-900 opacity-75 fixedw z-10 ">
-    <span class="text-amber-400 font-['Bitter'] text-4xl">Ertürk Mobilya</span>
-    <nav class="flex justify-right space-x-4">
+  <header class="flex bg-gray-900 px-4">
+    <div class="text-amber-400 font-['Bitter'] text-4xl">Ertürk Mobilya</div>
+    <nav class="flex flex-wrap">
       <a href="/" class="font-bold px-3 py-2 text-amber-400 rounded-lg hover:bg-slate-100 hover:text-slate-900 ">Ana Sayfa</a>
       <a href="/Projeler" class="font-bold px-3 py-2 text-amber-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projeler</a>
       <a href="/hakkimizda" class="font-bold px-3 py-2 text-amber-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">Hakkımzda</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 
   </script>
 
-  <section class="w-[1200px] mx-auto">
+  <section class="w-full md:w-[1200px] mx-auto">
   <Swiper
     spaceBetween={50}
     slidesPerView={1}
@@ -29,66 +29,76 @@
   </Swiper>
 </section>
 
-<div class=" p-5 h-30 relative opacity-50  m-[90px]">
-    <p class="text-center text-3xl font-serif ...">ERTÜRK MOBİLYA</p>
-    <p class="text-center text-1xl font-serif ... p-2 italic" >1970'den günümüze işimizi tutku ile yapıyoruz..</p>
+<div class=" p-16">
+    <p class="text-center text-3xl font-serif text-zinc-600">ERTÜRK MOBİLYA</p>
+    <p class="text-center text-1xl font-serif p-2 italic" >1970'den günümüze işimizi tutku ile yapıyoruz..</p>
 </div>
 
 
-<div class="container mx-auto flex space-x-4 ... p-18 h-111  ">
-    <div class="relative self-end">
-      <img src="mimari_planlama2.png" class="card-img-top rounded-full" alt="...">
-      <div class="mx-8 ...">
-        <h5 class="text-center m-2 opacity-60 font-serif ... text-2xl mt-6 bg-gray-100 text-black-200 rounded-full ...">Mimari Planlama</h5>
-      </div>
-    </div>
-    <div class="relative self-top">
-      <img src="mobilya_uretim.jpeg" class="rounded-full" alt="...">
-      <div class="mx-8 ...">
-        <h5 class="text-center text-2xl mt-6 font-serif ...
-        bg-gray-100 opacity-60 text-black-200 rounded-full ...">Mobilya Üretim</h5>
-      </div>
-    </div>
-    <div class="relative self-end">
-      <img src="montaj.jpeg" class="card-img-top rounded-full" alt="...">
-      <div class="mx-8 ...">
-        <h5 class="text-center m-2 opacity-60 font-serif ... text-2xl mt-6 bg-gray-100 text-black-200 rounded-full ...">Montaj</h5>    
-      </div>
-    </div>
+<div class="flex flex-col md:flex-row text-center px-6">
+  <div class="px-4 pb-16 md:pb-4">
+    <img class="rounded-full" src="/mimari_planlama2.png" alt="">
+    <h3 class="text-2xl text-zinc-600 font-serif">Mimari Planlama</h3>
+  </div>
+  <div class="px-4 pb-16 md:pb-4">
+    <img class="rounded-full" src="/mobilya_uretim.jpeg" alt="">
+    <h3 class="text-2xl text-zinc-600 font-serif">Mobilya Üretim</h3>
+  </div>
+  <div class="px-4 pb-16 md:pb-4">
+    <img class="rounded-full" src="/montaj.jpeg" alt="">
+    <h3 class="text-2xl  text-zinc-600 font-serif">Montaj</h3>
+  </div>
 </div>
 
-<section class="" id="middle-section">
-  <div class=" w-full text-center  font-roboto mt-20  border pr-48 pl-48 opacity-70 text-black-200">
-    <p class="font-roboto mt-2 mb-2 italic opacity-70 text-black-200">1970’den günümüze sürekli güncellenen altyapısı, 1000m2 üretim alanına sahip atölyemizde kaliteli mobilya ürünleri üretmekteyiz.
+<section class="mt-6 mb-6 p-6" id="middle-section">
+  <div class=" ">
+    <p>1970’den günümüze sürekli güncellenen altyapısı, 1000m2 üretim alanına sahip atölyemizde kaliteli mobilya ürünleri üretmekteyiz.</p>
 
-    50 yıla yakın tecrübesiyle, banka, otel, zincir cafe ve restaurant dekorasyonlarıyla birlikte, mutfak, banyo, ev dekorasyonu ve tadilat işlerini de müşteri beklentilerine uygun olarak başarıyla gerçekleştirmekteyiz.
+    <p>    50 yıla yakın tecrübesiyle, banka, otel, zincir cafe ve restaurant dekorasyonlarıyla birlikte, mutfak, banyo, ev dekorasyonu ve tadilat işlerini de müşteri beklentilerine uygun olarak başarıyla gerçekleştirmekteyiz.
+    </p>
     
-    İşlevsellik, kalite ve müşteri memnuniyetini ilke edinen firmamız yaşam alanlarına mutluluk getirmeye devam etmektedir.
-  </p>
+<p>    İşlevsellik, kalite ve müşteri memnuniyetini ilke edinen firmamız yaşam alanlarına mutluluk getirmeye devam etmektedir.
+</p>  
   </div>
 </section>
 
 
-<div class="p5 h-30 relative opacity-50  m-[90px] ">
-  <p class="text-center underline underline-offset-8 text-3xl font-serif ..."> <a href="/projeler">Bitirdiğimiz Projeler</a></p>
+<div class="p5 h-30 relative opacity-50  m-[90px] text-center">
+  <h3 class="text-3xl pb-4">Bitirdiğimiz Projeler</h3>
+  <a href="/projeler">Tümü</a>
 </div>
-   
+
+<div class="flex flex-col md:flex-row text-center justify-items-center space-x-10  px-6">
+  <div class="basis-1/2 md:basis-1/3">
+    <img class="" src="/mimari_planlama2.png" alt="">
+    <h3 class="text-2xl text-zinc-600 font-serif">Mimari Planlama</h3>
+  </div>
+  <div class="basis-1/2 md:basis-1/3">
+    <img class="" src="/mobilya_uretim.jpeg" alt="">
+    <h3 class="text-2xl text-zinc-600 font-serif">Mobilya Üretim</h3>
+  </div>
+  <div class="basis-1/2 md:basis-1/3">
+    <img class="" src="/montaj.jpeg" alt="">
+    <h3 class="text-2xl  text-zinc-600 font-serif">Montaj</h3>
+  </div>
+</div>
+
 
 <div class="container flex  text-center  mx-auto ">
   <div class="relative p-2  mb-10 b ">
-  <img src="/proje1.png" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Asia Artemis Suite <br> Ataşehir</h2>
+  <img src="/proje1.png" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Asia Artemis Suite <br> Ataşehir</h2>
 
 </div>
 
 <div class="relative p-2 mb-10">
-  <img src="lamec.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Lamec Hotel <br> Gebze</h2>
+  <img src="lamec.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Lamec Hotel <br> Gebze</h2>
 
 </div>
 <div class="relative p-2 mb-10">
-  <img src="retaj.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Retaj Termal Hotel <br> Yalova</h2>
+  <img src="retaj.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Retaj Termal Hotel <br> Yalova</h2>
   
 </div>
 </div>
@@ -96,39 +106,44 @@
    
 <div class="container flex  text-center  mx-auto ">
   <div class="relative p-2  mb-10 b ">
-  <img src="my_homes_hotel_pendik.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">MYY Homes Hotel <br> Pendik</h2>
+  <img src="my_homes_hotel_pendik.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">MYY Homes Hotel <br> Pendik</h2>
 
 </div>
 
 <div class="relative p-2 mb-10">
-  <img src="grand_korfez_hotel_erturkmobilya.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Grand Körfez Hotel <br> Gebze</h2>
+  <img src="grand_korfez_hotel_erturkmobilya.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Grand Körfez Hotel <br> Gebze</h2>
 
 </div>
 <div class="relative p-2 mb-10">
-  <img src="sisli_vizon_hotel_mobilya_dekorasyon_erturk_mobilya.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Vizon Hotel <br>Şişli</h2>
+  <img src="sisli_vizon_hotel_mobilya_dekorasyon_erturk_mobilya.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Vizon Hotel <br>Şişli</h2>
   
 </div>
 </div>
 
 <div class="container flex  text-center  mx-auto ">
   <div class="relative p-2  mb-10 b ">
-  <img src="salon_serenay_erturk_mobilya.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Serenay Düğün Salonu <br> İçerenköy</h2>
+  <img src="salon_serenay_erturk_mobilya.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Serenay Düğün Salonu <br> İçerenköy</h2>
 
 </div>
 
 <div class="relative p-2 mb-10">
-  <img src="old_city_hotel_sultanahmet_istanbul_mobilya_erturk.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Old City Hotel <br> Sultanahmet</h2>
+  <img src="old_city_hotel_sultanahmet_istanbul_mobilya_erturk.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Old City Hotel <br> Sultanahmet</h2>
 
 </div>
 <div class="relative p-2 mb-10">
-  <img src="asia_royal_suite_maltepe_mobilya_erturk.jpeg" alt="" class="rounded-lg ...">
-  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ...">Asia Royal Suite Hotel Maltepe</h2>
+  <img src="asia_royal_suite_maltepe_mobilya_erturk.jpeg" alt="" class="rounded-lg ">
+  <h2 class="text-center opacity-60 text-black-200 mt-2 text-2xl font-serif ">Asia Royal Suite Hotel Maltepe</h2>
   
 </div>
 </div>
 
+<style>
+  p {
+    @apply pt-2 pb-2
+  }
+</style>
