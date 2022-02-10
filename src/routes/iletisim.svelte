89003ff33b005abd5eb197c098694673dc4fb2fb
alp61 <alpozturk61@hotmail.com>
2022-02-10 13:19:58 +0300

    update contact us

--- src/routes/iletisim.svelte
+++ src/routes/iletisim.svelte
@@ -1,6 +1,28 @@
-<h1>iletişim</h1>
-<form action="#">
-    <input class="border-2" type="email" name="email" id="email" placeholder="Type your email" required>
-    <input class="border-2"  type="text" name="name" id="name" placeholder="Type your name">
-    <input class="border-2"  type="submit" value="Gönder">
-</form>
+    <div class="flex w-full min-h-screen justify-center items-center bg-gray-500">  
+         <div 
+         class="bg-cyan-500 w-full flex-col space-y-6 max-w-4xl p-8 rounded-xl shadow-lg text-white"
+         >
+         <div class="flex flex-col justify-between space-y-8"> 
+
+            <div>
+                <h1 class="font-bold text-4xl tracking-wide">İletişim Formu</h1>
+                <p class="pt-2 text-sm"> Bizimle iletişime geçebilmek için lütfen gerekli alanları doldurunuz</p>
+            </div>
+            <div>
+            <div class="inline-flex space-x-2 items-center">
+                <i name="call" class="fas fa-phone-square-alt text-xl"></i>
+                <a href="/"><span>0262 658 29 23</span></a>
+            </div>
+        </div>
+            <div class="inline-flex space-x-2 items-center">
+                <i name="call" class="fas fa-phone-square-alt text-xl"></i>
+                <a href="/"><span>0262 658 29 23</span></a>
+            </div>
+            <div></div>
+
+         </div>
+         <div>
+
+        </div>
+        </div>
+    </div>
