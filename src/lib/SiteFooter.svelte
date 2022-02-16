4804a113d964d2143d2afcd9d6db34acdc5b9093
Ozgur Dincer <ozgurdincer@kentajans.com>
2022-02-16 23:09:34 +0300

    fix

---
+++ src/lib/SiteFooter.svelte
@@ -0,0 +1,12 @@
+<footer class="h-auto w-full font-serif text-white leading-[3rem] bg-gray-800 opacity-60 item bottom-0">
+    <div class="footer-content flex items-center justify-center flex-col">
+        <h3 class="bottom-0"> ERTÜRK MOBİLYA</h3>
+        <ul class="social-links items-center flex justify-center">
+            <li><a href="/"><i class="fa fa-facebook list-none mx-6 hover:text-blue-600 delay-100 duration-700"></i></a></li>
+            <li><a href="/"><i class="fa fa-instagram list-none mx-6 hover:text-pink-600 delay-100 duration-700"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom text-center">
+        <p>Copyright &copy;2022 / Designed by <span> A Solutions</span></p>
+    </div>
+</footer>
