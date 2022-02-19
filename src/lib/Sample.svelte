8310451cb95ac76b61ca45187a3bc0fcc8405e62
alp61 <alpozturk61@hotmail.com>
2022-02-19 16:27:14 +0300

    portfolio-map

--- src/lib/Sample.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let name = "Alper";
-</script>
-
-<h2>Merhaba {name}</h2>
--- src/routes/projeler.svelte
+++ src/routes/projeler.svelte
@@ -4,7 +4,7 @@
 
 
 <div class="p5 h-30 relative opacity-50  m-[90px] text-center">
-    <h3 class="text-3xl pb-4 font-serif">Referanslarımız</h3>
+    <h3 class="text-3xl pb-4 font-serif self-center text-center">Referanslarımız</h3>
     <!-- svelte-ignore a11y-missing-content -->
     <a href="/projeler"></a>
 </div>
